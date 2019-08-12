629e1686831dfcf3b3b8699149d570dccb981761
aniduvvuru <aniduvvuru@gmail.com>
2019-08-12 10:56:13 -0500

    Gender breakdown added

--- practice0.R
+++ practice0.R
@@ -5,4 +5,22 @@ rm(list = ls())
 math_phds = as.data.table(AMSsurvey)
 summary(math_phds)
 math_phds = math_phds[,-1]
-math_phds
+
+# PHD totals by gender
+
+Y2018 = math_phds[,.(count.sum=sum(count)), by=sex]
+Y2019 = math_phds[,.(count11.sum=sum(count11)), by=sex]
+math_by_sex = merge(Y2018, Y2019)
+math_by_sex
+
+# Y2y PHD growth by gender
+
+gen_lm = lm(count11 ~ count, data=math_phds)
+male_math = math_phds[sex=="Male"]
+male_lm =lm(count11 ~ count, data=male_math)
+fem_math = math_phds[sex=="Female"]
+fem_lm =lm(count11 ~ count, data=fem_math)
+
+gen_lm
+male_lm
+fem_lm
