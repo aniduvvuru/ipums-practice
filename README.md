d151718517a2492e00b0360246d00d1ccd5c5d09
aniduvvuru <aniduvvuru@gmail.com>
2019-08-13 16:59:19 -0500

    Continued updating practice1 with new data.table methods
    Changed README.md to include new lessons about R functions as well as keyboard shortcuts

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # ipums-practice
 
-Written from the terminal in RStudio
+Written in the terminal from Rstudio.
+
+LESSONS
+help(":=") -> new column by reference operator
+help(data.table) -> dt[i,j,by] + other methods explained (.SD)
+help(unique) -> grabs unique rows on the basis of certain chosen columns
+help(lapply) -> "list apply"; apply a function over a list of vectors
+help(sapply) -> "simplify and apply"; lapply that simplifies result to vector/matrix if possible
+help(mapply) -> sapply used on multiple list or vector arguments
+help(rep) -> repeats many items 
+help(setdiff) -> order-dependent; return a new set containing only values unique to first set 
+help(setequal) -> returns a boolean based on set equivalence 
+help(`%>%`)
+	x %>% f(y) places x in the leftmost arugment position -> f(x,y)
+    	Position can be chosen with dot replacement -> x %>% f(y,.) -> f(y,x)
+    	Nested functions include x at each scope -> x %>% g(f(y,.)) -> g(x, f(y,x))
+    	Nesting can be overriden with {} enclosure -> x %>% {g(f(y,.))} -> g(f(y,x))
+    	Function-producing RHS can be forced to evaluate before piping w/ () enclosure
+
+SHORTCUTS
+ctrl + D = delete line
+alt + shift + left/right = select rest of line in that direction
+ctrl + shift + A  = format all code
+ctrl + shift + M = Magrittr's Pipe
--- practice1.R
+++ practice1.R
@@ -1,32 +1,49 @@
-# Lessons
-# help(":=") -> new column by reference operator
-# help(data.table) -> dt[i,j,by] + other methods explained (.SD and lapply)
-# help(unique) -> grabs unique rows on the basis of certain chosen columns
-# help(lapply) -> "list apply"; apply a function over a list of vectors
-# help(sapply) -> "simplify and apply"; lapply that converts to vector/matrix if possible
-#   and a mulit-dim array at request
-# help(mapply) -> sapply used on multiple list or vector arguments
-# help(rep) -> repeats many items
-# help(setdiff) -> order-dependent; return a new set containing only values unique to first set
-# help(setequal) -> returns a boolean based on set equivalence
-
+library(tidyverse)
 library(data.table)
-library(dplyr)
+rm(list = ls())
 
 # Summarize unique gears available to each cylinder type
-dt = data.table(mtcars)[,.(gear, cyl)]
-dt[,gearsL := .(list(unique(gear))), by=cyl]
+dt = data.table(mtcars)[, .(gear, cyl)]
+dt[, gearsL := .(list(unique(gear))), by = cyl]
 head(dt)
 
 # Grab list item from column of list values
-dt[,gearL1:=lapply(gearsL, `[`, 2)]
-dt[,gearS1:=sapply(gearsL, `[`, 2)]
+dt[, gearL1 := lapply(gearsL, `[`, 2)]
+dt[, gearS1 := sapply(gearsL, `[`, 2)]
 head(dt)
-str(head(dt[,gearL1]))
-str(head(dt[,gearS1])) # Note: sapply -> vector vs lapply -> list
+str(head(dt[, gearL1]))
+str(head(dt[, gearS1])) # Note: sapply -> vector vs lapply -> list
 
-dt[,other_gear:=mapply(setdiff, gearsL, gear)]
+dt[, other_gear := mapply(setdiff, gearsL, gear)]
 head(dt)
 
 # Suppressing intermediate output
+dt = data.table(mtcars)
+dt[, {
+  tmp1 = mean(mpg)
+  tmp2 = mean(abs(mpg - tmp1))
+  tmp3 = round(tmp2, 2)
+}, by = cyl]
+dt[, {
+  tmp1 = mean(mpg)
+  tmp2 = mean(abs(mpg - tmp1))
+  tmp3 = round(tmp2, 2)
+  list(tmp2 = tmp2, tmp3 = tmp3)
+}, by = cyl]
+
+# Faster looping  with set
+## Applying function to specific columns
+dt2 = dt[, 1:5]
+for (j in c(1L, 2L, 3L))
+  set(dt2, j = j, value = -dt2[[j]])
+for (j in c(1L, 2L, 3L))
+  set(dt2, j = j, value = paste0(-dt2[[j]], '!!'))
+dt
+dt2
+
+# Shift to lead/lag vectors and lists
+dt = data.table(mtcars)[, .(mpg, cyl)]
+dt[, mpg_lag1 := shift(mpg, 1)]
+dt[, mpg_forward1 := shift(mpg, 1, type = 'lead')]
+head(dt)
 
